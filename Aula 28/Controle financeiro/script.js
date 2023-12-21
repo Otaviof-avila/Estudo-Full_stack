5bf92a6448a08e8a17eaaf9026d40f8757f71556
Otaviof-avila <otaviofernandesdeavila16@gmail.com>
2023-12-21 16:40:21 -0300

    Correção no codigo

--- Aula 28/Controle financeiro/script.js
+++ Aula 28/Controle financeiro/script.js
@@ -1,13 +1,14 @@
-saldoInicial = parseFloat(prompt("Digite o saldo inicial:"))
-novoValor = 0
-operacao = ""
+let saldoInicial = parseFloat(prompt("Informe o saldo inicial:"))
+let novoValor = 0
+let operacao = ""
 
 do {
     operacao = prompt(
-        "Escollha uma das opções:\n"+
-        "1. Adicionar dinheiro\n"+
-        "2. Subtrair dinheiro\n"+
-        "3. Encerrar"
+
+        "Saldo disponivel: R$ "+saldoInicial+
+        "\n1. Adicionar dinheiro"+
+        "\n2. Subtrair dinheiro"+
+        "\n3. Encerrar"
     )
 
     switch (operacao) {
@@ -18,12 +19,12 @@ do {
             break;
         case "2":
             novoValor = parseFloat(prompt(" Digite o valor que vai ser subtraido:"))
-            saldoInicial -= novoValor
+            saldoInicial -= novgtioValor
             alert("O novo saldo é de "+ saldoInicial)
             break;
     
         default:
             break;
     }
-    
+
 } while (operacao !== "3");
